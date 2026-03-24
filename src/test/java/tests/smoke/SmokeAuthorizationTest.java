f35cf83f3939e7c54c7e3f5a7e907f64d3f45c48
olgasenaeva <ovshenaeva@gmail.com>
2026-03-24 23:06:03 +0300

    Update test SmokeAuthorizationTest2

--- src/test/java/tests/smoke/SmokeAuthorizationTest.java
+++ src/test/java/tests/smoke/SmokeAuthorizationTest.java
@@ -1,5 +1,6 @@
 package tests.smoke;
 
+import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.options.AriaRole;
 import config.Credentials;
@@ -9,6 +10,7 @@ import core.tags.RunTags;
 import org.junit.jupiter.api.Test;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
 
 @RunTags.UI
 @RunTags.Smoke
@@ -35,11 +37,40 @@ public class SmokeAuthorizationTest extends BaseTest {
        // assertThat(page.getByRole(AriaRole.LINK,
         //        new Page.GetByRoleOptions().setName(Credentials.email()))).isVisible();
 
-        assertThat(page.getByRole(AriaRole.LINK,
-                new Page.GetByRoleOptions().setName("My account"))).isVisible();
-        assertThat(page.locator(".ico-logout")).isVisible();
+        //assertThat(page.getByRole(AriaRole.LINK,
+        //        new Page.GetByRoleOptions().setName("My account"))).isVisible();
+        // assertThat(page.locator(".ico-logout")).isVisible();
+
+        Locator logout = page.locator(".ico-logout");
+        Locator myAccount = page.getByRole(AriaRole.LINK,
+                new Page.GetByRoleOptions().setName("My account"));
+        Locator validationSummary = page.locator(".validation-summary-errors");
+        Locator emailError = page.locator("[data-valmsg-for='Email']");
+        Locator passwordError = page.locator("[data-valmsg-for='Password']");
+
+        page.waitForTimeout(2000);
+
+        if (validationSummary.isVisible()) {
+            fail("Login failed. Validation summary: " + validationSummary.textContent());
+        }
+
+        if (emailError.isVisible()) {
+            fail("Login failed. Email error: " + emailError.textContent());
+        }
+
+        if (passwordError.isVisible()) {
+            fail("Login failed. Password error: " + passwordError.textContent());
+        }
+
+        if (!logout.isVisible() && !myAccount.isVisible()) {
+            fail("Login did not succeed. Current URL: " + page.url()
+                    + " | Page title: " + page.title());
+        }
 
         // 7. Нажать на кнопку Log out
+        assertThat(myAccount).isVisible();
+        assertThat(logout).isVisible();
+
         page.locator(".ico-logout").click();
 
         // 8. Проверить разлогин
